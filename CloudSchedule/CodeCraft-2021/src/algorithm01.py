2e275ffe95eeea9769aeb62cfe33df257326435f
BlueStragglers <xczhou2019@gmail.com>
2021-03-12 16:31:18 +0800

    完成 algorithm 逻辑功能部分

--- CloudSchedule/CodeCraft-2021/src/algorithm01.py
+++ CloudSchedule/CodeCraft-2021/src/algorithm01.py
@@ -24,34 +24,108 @@ class algorithm(object):
         self.cost = 0
         # 当前 server_id
         self.server_id = 0
-        # 格式：[server_type_dict : vm_type_dict]
-        self.server_for_vm = []
+        # 格式：{vm_id : server_id}
+        self.vm_map = {}
+        # 格式：{request_item_vm_type : server_type_dict}
+        self.best_server_for_vm = {}
+        # 格式：{vm_name : vm_deployment_way}
+        self.vm_is_or_not_partition = {}
 
-        self.process_daily_queue()
+        self.deal_with_daily_queue()
 
-    def process_daily_queue(self):
+    def deal_with_daily_queue(self):
+        self.get_best_server_for_vm()
+        self.get_vm_is_or_not_partition()
+
+        # 构建输出列表
+        purchase_list = []
+        migration_list = []
+        create_list = []
+        delete_list = []
+
         for daily_queue in self.daily_queue_list:
+            for request in daily_queue.get_daily_queue_info():
+
+                # 添加虚拟机
+                if request["request_item_action"] == "add":
+                    flag = False
+                    server_id = -1
+
+                    # 首先检查是否存在未使用的最佳服务器
+                    for server in self.idle_server_list:
+                        if self.server_map[server] == self.best_server_for_vm[request["request_item_vm_type"]]:
+                            flag = True
+                            server_id = server
+                            break
+
+                    # 存在未使用服务器，拿来用
+                    if flag:
+                        self.idle_server_list.remove(server_id)
+                        self.work_server_list.append(server_id)
+
+                    # 不存在，购买新服务器
+                    else:
+                        best_server = self.best_server_for_vm[request["request_item_vm_type"]]
+                        flag = False
+                        j = -1
+                        for i in purchase_list:
+                            if best_server in i:
+                                flag = True
+
+                        # 加入购买队列
+                        if flag:
+                            purchase_list[j][1] += 1
+                        else:
+                            purchase_list.append((best_server, 1))
+                        server_id = self.server_id - 1
+
+                    # 加入添加队列
+                    create = [server_id]
+                    if self.vm_is_or_not_partition[request["request_item_vm_type"]] == 0:
+                        create.append("A")
+                    create_list.append(create)
 
 
+                # 删除虚拟机
+                else:
+                    del_vm_server_id = self.vm_map.pop(request["request_item_vm_id"])
+                    self.work_server_list.remove(del_vm_server_id)
+                    self.idle_server_list.append(del_vm_server_id)
+
+        # 执行输出
+        self.purchase(purchase_list)
+        self.migration(migration_list)
+        self.create(create_list)
+        self.delete(delete_list)
+
+    def get_vm_is_or_not_partition(self):
+        for vm in self.vm_type_list:
+            self.vm_is_or_not_partition["{}".format(vm["vm_name"])] = vm["vm_deployment_way"]
 
     def get_best_server_for_vm(self):
+        # 对服务器分别按照 价格， cpu 和 memory 排序
+        # server_sort_by_cpu = sorted(self.server_type_list, key=lambda x: x["server_cpu_num"])
+        # server_sort_by_memory = sorted(self.server_type_list, key=lambda x: x["server_memory_size"])
+        server_sort_by_hardware_cost = sorted(self.server_type_list, key = lambda x : x["server_hardware_cost"], reverse = True)
 
-        server_sort_by_cpu = sorted(self.server_type_list, key = lambda x : x["server_cpu_num"])
-        server_sort_by_memory = sorted(self.server_type_list, key = lambda x : x["server_memory_size"])
+        for vm in self.vm_type_list:
 
-
-
+            # 这里需要根据 vm 是否是双节点做一个判断
+            denominator = 1 if vm["vm_deployment_way"] == 1 else 2
+            for server in server_sort_by_hardware_cost:
+                if server["server_cpu_num"] / denominator >= vm["vm_cpu_num"] and server["server_memory_num"] / denominator >= vm["vm_memory_num"]:
+                    self.best_server_for_vm["{}".format(vm["vm_name"])] = server
 
 
     def purchase(self, purchase_list):
         """
         购买服务器
-        :param purchase_list: 购买列表。类型为 [(server_name, num)]
+        :param purchase_list: 购买列表。类型为 [(server_type_dict, num)]
         """
         print("(purchase, {})".format(len(purchase_list)))
 
         for server in purchase_list:
-            print("({}, {})".format(server[0], server[1]))
+            print("({}, {})".format(server[0]["server_name"], server[1]))
 
             for i in range(server[1]):
                 self.server_list.append(self.server_id)
@@ -62,7 +136,6 @@ class algorithm(object):
             for s in self.server_type_list:
                 if s["server_name"] == server[0]:
                     self.cost += s["server_hardware_cost"]
-
 
     def migration(self, migration_list):
         """
@@ -78,9 +151,6 @@ class algorithm(object):
             else:
                 print("({}, {})".format(migration[0], migration[1]))
 
-
-
-
     def create(self, create_list):
         """
         创建虚拟机
@@ -92,17 +162,11 @@ class algorithm(object):
             else:
                 print("({})".format(create[0]))
 
-
-
-
-
     def delete(self, delete_list):
         """
         删除虚拟机
         :param delete_list:删除列表。类型为 []
         """
-
-
 
     def get_cost(self):
         """
@@ -115,6 +179,7 @@ class algorithm(object):
 def algorithm01():
     algorithm_result = algorithm()
     return algorithm_result.get_cost()
+
 
 def get_result():
     cost = algorithm01()
