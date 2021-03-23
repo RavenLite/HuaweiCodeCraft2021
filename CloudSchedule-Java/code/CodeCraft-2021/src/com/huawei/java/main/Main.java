98486774bb0dbefdc1c66fa71e0374d647e64ad1
BlueStragglers <xczhou2019@gmail.com>
2021-03-23 21:33:07 +0800

    commit

--- CloudSchedule-Java/code/CodeCraft-2021/src/com/huawei/java/main/Main.java
+++ CloudSchedule-Java/code/CodeCraft-2021/src/com/huawei/java/main/Main.java
@@ -1,11 +1,31 @@
 package com.huawei.java.main;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // TODO: read standard input
         // TODO: process
         // TODO: write standard output
         // TODO: System.out.flush()
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("./CloudSchedule/training_data/training-1.txt"));
 
+            int server_type_num = Integer.parseInt(in.readLine());
+
+
+            String str;
+            int i = 0;
+            while ((str = in.readLine()) != null) {
+                i++;
+                System.out.println(str);
+            }
+            System.out.println(i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
