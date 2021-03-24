a19b5c59a37700899c3fa6e2c43ffedd79a5601c
BlueStragglers <xczhou2019@gmail.com>
2021-03-24 20:18:17 +0800

    Complete Output, Read, ReadFile part

--- CloudSchedule-Java/code/CodeCraft-2021/src/utils/Output.java
+++ CloudSchedule-Java/code/CodeCraft-2021/src/utils/Output.java
@@ -5,30 +5,38 @@ import pojo.*;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 进行输出
+ */
 public class Output {
+
     private static final Constant constant = new Constant();
 
     public void output_daily(int purchase_server_num, HashMap<ServerType, Integer> purchase_server_list,
                              int migrate_vm_num, HashMap<Vm, Server> migrate_vm_list,
                              DailyQueue daily_queue_list) {
+
+        // 输出购买
         System.out.printf("(purchase, %d)%n", purchase_server_num);
         for (Map.Entry<ServerType, Integer> entry : purchase_server_list.entrySet()) {
             System.out.printf("(%s, %d)%n", entry.getKey().getServerTypeName(), entry.getValue());
         }
+
+        // 输出迁移
         System.out.printf("(migration, %d)%n", migrate_vm_num);
 //        for (Map.Entry<Vm, Server> entry : migrate_vm_list) {
 //            System.out.printf("");
 //        }
 
-//        for (QueueItem queue_item : daily_queue_list.getQueueItemList()) {
-//            if (queue_item.getQueueItemAction().equals(constant.ACTION_ADD)) {
-//                if (queue_item.getQueueItemVmType().getVmTypeDeploymentWay() == constant.VM_DEPLOYMENT_SINGLE) {
-//                    System.out.printf("(%d)%n", queue_item.getQueueItemVmType().);
-//                } else {
-//                    System.out.printf("(%d, %d)%n", );
-//                }
-//            }
-//        }
-
+        // 输出部署服务器
+        for (QueueItem queue_item : daily_queue_list.getQueueItemList()) {
+            if (queue_item.getQueueItemAction().equals(constant.ACTION_ADD)) {
+                if (queue_item.getQueueItemVmType().getVmTypeDeploymentWay() == constant.VM_DEPLOYMENT_SINGLE) {
+                    System.out.printf("(%d, %s)%n", queue_item.getQueueVm().getServerId(), queue_item.getQueueVm().getDeployNode());
+                } else {
+                    System.out.printf("(%d)%n", queue_item.getQueueVm().getServerId());
+                }
+            }
+        }
     }
 }
